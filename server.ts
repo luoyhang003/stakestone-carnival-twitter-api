450254fab1411bd3062fbc42f3c4343aa8da6c44
luoyhang003 <luoyhang003@Hotmail.com>
2024-03-04 19:18:00 +0800

    update API endpoint

--- server.ts
+++ server.ts
@@ -9,8 +9,8 @@ dotenv.config();
 const app = express();
 
 const STAKE_STONE_ID = "1657625619474284546";
-const SERVER_URL = "http://localhost:8064/stone/integral/save"
-const CALLBACK_URL = "http://localhost:3000/callback";
+const SERVER_URL = "https://api.stakestone.io:8064/stone/integral/save"
+const CALLBACK_URL = "https://api.stakestone.io:3000/callback";
 const CLIENT_ID = process.env.CLIENT_ID as string;
 const CLIENT_SECRET = process.env.CLIENT_SECRET as string;
 const DEST_TWEET_ID = "1761788204745904498";
